be7400183e56e86da7e9c1c16c13ca91eb92a20b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-06 10:57:58 -0500

    [+] hero.service: mock-heroes retorna como Observable & Implementa una subscripcion a dicho service desde el componente HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
 
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
-  heroes = HEROES;
+  heroes!: Hero[];
   selectedHero!: Hero;
+
+  constructor( private heroService: HeroService ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   /** Agregamos controlador de eventos de click */
   onSelect( hero: Hero ): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe( heroes => this.heroes = heroes );
+  }
+
 }
