94cd12427a770dd82d5ee1e45180cdfa481960a2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-11 09:28:51 -0500

    [+] hero.service: Integra MessageService a HeroService agregando un mensaje cuando se obtienen Heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,15 +3,20 @@ import { Observable, of } from 'rxjs';
 
 import { HEROES } from './mock-heroes'
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor( private messageService: MessageService) { }
 
   getHeroes(): Observable< Hero[] > {
-    return of( HEROES );
+    const heroes = of( HEROES );
+
+    this.messageService.add( 'HeroService: fetched heroes' );
+
+    return heroes;
   }
 }
